c96831aa5f50a5627cdf2095450e9615fbc3908e
zhanghong <zhanghong@networking.io>
2015-11-02 16:44:48 +0800

    init project

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# kit = IMGKit.new("http://skshu.zhanghong.com/Product/index/category/zhuangshi.html", :quality => 100, :crop_w => 1200)
+# file = kit.to_file(File.join(Rails.root, "public/image_1.jpg"))
